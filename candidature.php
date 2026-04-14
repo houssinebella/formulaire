1ad9a9a19af97da4c5c5579d93fc64818653b88b
houssinebella <houssinebella2019@gmail.com>
2026-04-14 13:37:42 +0100

    feat: formulaire HTML avec tous les champs

--- candidature.php
+++ candidature.php
@@ -10,12 +10,38 @@ $erreurs   = [];
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulaire de candidature</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-</body>
-</html>
+
+<h2>Formulaire de candidature</h2>
+<form action="candidature.php" method="POST">
+
+    <label for="prenom">prenom:</label>
+    <input type="text" name="prenom" >
+
+     <label for="nom">nom:</label>
+    <input type="text" name="nom" >
+
+     <label for="email">email:</label>
+    <input type="email" name="email" >
+
+     <label for="age">age:</label>
+    <input type="number" name="age" >
+
+     <label for="filiere">filiere:</label>
+    <select name="filiere" id="filiere">
+    <option value="pc">physique</option>
+    <option value="math">mathematique</option>
+    <option value="si">science de lingenieur</option>
+    <option value="autre">autre</option>
+    </select>
+
+    <label for="motivation">motivation:</label>
+    <textarea name="motivation" rows="6"></textarea>
+
+    <label for="reglement"></label>
+    </form>
+    </body>
